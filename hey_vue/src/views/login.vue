09e772d526abb794d9de64b10356e0f3fb6bdc11
冯伽略 <fengjl7@yonyou.com>
2019-06-29 09:55:08 +0800

    feat:登录功能

---
+++ hey_vue/src/views/login.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>请登录</h1>
+        用户名：<input type="text" v-model="username"/>
+        密码：<input type="password" v-model="password"/>
+        <button @click="login">登录</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return{
+            username:"",
+            password:""
+        }
+    },
+    methods:{
+        login(){
+            axios.get("../assets/json/users.json").then(function(response){
+                window.console.log(response);
+            }).cacth(function(error){
+                window.console.log(error);
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
